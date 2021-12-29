cafa4da48f06807debf48c18f61f22d6db420cc9
kirlie <96751918+kirlie@users.noreply.github.com>
2021-12-29 20:13:16 -0300

    agrego funcion para la suma

--- calculadora.py
+++ calculadora.py
@@ -5,3 +5,19 @@ print("3 - MULTIPLICACION")
 print("4 - DIVISION")
 
 opcion = int(input("Elija la opcion"))
+
+def suma(a, b):
+    return (a + b)
+
+if(opcion == 1):
+    a = int(input("Ingrese el primer numero"))
+    b = int(input("Ingrese el segundo numero"))
+    print(suma(a, b))
+elif(opcion == 2):
+    print("opcion 2")
+elif(opcion == 3):
+    print("opcion 3")
+elif(opcion == 4):
+    print("opcion 4")
+else:
+    print("Elija una opcion entre 1 a 4")
