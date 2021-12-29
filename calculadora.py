7029f9a9a73d858ee7e4071d127a14a18b8991c7
kirlie <96751918+kirlie@users.noreply.github.com>
2021-12-29 20:20:02 -0300

    agrego funciones resta, multiplicacion y division

--- calculadora.py
+++ calculadora.py
@@ -9,15 +9,32 @@ opcion = int(input("Elija la opcion"))
 def suma(a, b):
     return (a + b)
 
+def resta(a, b):
+    return (a - b)
+    
+def multiplicacion(a, b):
+    return (a * b)
+    
+def division(a, b):
+    return (a // b)
+
 if(opcion == 1):
     a = int(input("Ingrese el primer numero"))
     b = int(input("Ingrese el segundo numero"))
     print(suma(a, b))
 elif(opcion == 2):
-    print("opcion 2")
+    a = int(input("Ingrese el primer numero"))
+    b = int(input("Ingrese el segundo numero"))
+    print(resta(a, b))
 elif(opcion == 3):
-    print("opcion 3")
+    a = int(input("Ingrese el primer numero"))
+    b = int(input("Ingrese el segundo numero"))
+    print(multiplicacion(a, b))
 elif(opcion == 4):
-    print("opcion 4")
+    a = int(input("Ingrese el primer numero"))
+    b = int(input("Ingrese el segundo numero"))
+    if (b == 0):
+        print("No se puede dividir entre cero")
+    print(division(a, b))
 else:
     print("Elija una opcion entre 1 a 4")
