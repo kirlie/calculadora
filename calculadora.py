afe68947650baacde8f4b5fee8ec6fd511023925
kirlie <96751918+kirlie@users.noreply.github.com>
2021-12-29 20:10:50 -0300

    Creación del menu

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("Calculadora, elija una opción ingresando un número:")
+print("1 - SUMA ")
+print("2 - RESTA")
+print("3 - MULTIPLICACION")
+print("4 - DIVISION")
+
+opcion = int(input("Elija la opcion"))
